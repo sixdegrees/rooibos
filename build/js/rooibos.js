55d06a63e8794d6b9eee8bc527d4eb6d56e1e8b2
Nicolas Mérouze <nicolas.merouze@gmail.com>
2010-08-26 15:49:22 +0200

    Open content with a click on the arrow for outline

--- build/js/rooibos.js
+++ build/js/rooibos.js
@@ -23628,6 +23628,9 @@ function loading(text) {
   $.widget("ui.outline", $.ui.accordion, {
     _create: function() {
       this.options.autoHeight = false;
+      this.options.collapsible = true;
+      this.eventOption = this.options.event;
+      this.options.event = false;
       $.ui.accordion.prototype._create.apply(this);
       this.element.addClass("ui-outline");
       // Highlight selected element on hash change
@@ -23636,14 +23639,21 @@ function loading(text) {
         self.element.find(".ui-accordion-content a").removeClass("selected");
         var el = self.element.find(".ui-accordion-content a[href=" + location.hash + "]");
         el.addClass("selected");
-        self.options.animated = false;
-        self.activate(self.element.find(".ui-accordion-content").index(el.parent().parent().parent()));
-        self.options.animated = "slide";
+        // self.options.animated = false;
+        // self.activate(self.element.find(".ui-accordion-content").index(el.parent().parent().parent()));
+        // self.options.animated = "slide";
       }).hashchange();
+    },
+    
+    _createIcons: function() {
+      $.ui.accordion.prototype._createIcons.apply(this);
       
-      if (self.options.richHeaders) {
-        self.element.find(".ui-accordion-header a").click(function() {
-          window.location.hash = $(this).attr("href");
+      var self = this;
+      if (this.eventOption) {
+        self.headers.find("span.ui-icon").bind( this.eventOption.split(" ").join(".accordion ") + ".accordion", function(event) {
+          event.currentTarget = $(this).parent()[0];
+          self._clickHandler.call( self, event, $(this).parent()[0] );
+          event.preventDefault();
         });
       }
     }
