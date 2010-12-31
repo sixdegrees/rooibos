9b2abe70a0c30b8cf174ffd40627df18da087b33
Nicolas Mérouze <nicolas.merouze@gmail.com>
2010-12-31 15:19:54 +0100

    Fix widgets example

--- examples/widgets/js/application.js
+++ examples/widgets/js/application.js
@@ -1,58 +1,27 @@
-var app = Sammy(function() {
-  this.element_selector = "#content";
-  this.use("Rooibos");
+var Controller = Backbone.Controller.extend({
+  routes: {
+    "/": "index",
+    "/:section/:name": "show"
+  },
   
-  this.get("#/", function() {
-    this.partial("js/views/buttons/default.ms");
-  });
+  index: function() {
+    window.location = "#/buttons/default";
+  },
   
-  this.get("#/buttons/radios", function() {
-    this.partial("js/views/buttons/radios.ms");
-  });
+  show: function(section, name) {
+    this._update("#content", "js/views/" + section + "/" + name + ".html");
+  },
   
-  this.get("#/buttons/checkboxes", function() {
-    this.partial("js/views/buttons/checkboxes.ms");
-  });
-  
-  this.get("#/buttons/icons", function() {
-    this.partial("js/views/buttons/icons.ms");
-  });
-  
-  this.get("#/buttons/back", function() {
-    this.partial("js/views/buttons/back.ms");
-  });
-  
-  this.get("#/buttons/buttonbar", function() {
-    this.partial("js/views/buttons/buttonbar.ms");
-  });
-  
-  this.get("#/widgets/form-controls", function() {
-    this.partial("js/views/widgets/form_controls.ms");
-  });
-  
-  this.get("#/widgets/dialogs", function() {
-    this.partial("js/views/widgets/dialogs.ms");
-  });
-  
-  this.get("#/widgets/progress", function() {
-    this.partial("js/views/widgets/progress.ms");
-  });
-  
-  this.get("#/widgets/loading", function() {
-    this.partial("js/views/widgets/loading.ms");
-  });
-  
-  this.get("#/widgets/scrollbars", function() {
-    this.partial("js/views/widgets/scrollbars.ms");
-  });
-  
-  this.get("#/widgets/accordion", function() {
-    this.partial("js/views/widgets/accordion.ms");
-  });
-  
-  this.get("#/widgets/table", function() {
-    this.partial("js/views/widgets/table.ms");
-  });
+  _update: function(selector, templatePath) {
+    $(selector).load(templatePath, function() {
+      Rooibos.update(selector);
+    });
+  }
 });
 
-$(function() { app.run("#/"); });
+$(function() {
+  Rooibos.update("body");
+  new Controller();
+  Backbone.history.start();
+  window.location = "#/";
+});
